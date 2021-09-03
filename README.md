bd9c4395241c788664ea995c4695eb86e9a6adc8
Narsters <jruys@yahoo.com>
2021-09-02 23:11:59 -0600

    Return Object instead of JSON string

--- README.md
+++ README.md
@@ -1,23 +1,39 @@
-# Currently Testing this package, once testing is complete this line will be removed!!!!!!!!!!!
 # Node Adafruit PMSA300I Sensor Driver
 This node module allows to read data from Adafruit PMSA300I Air Quality sensor (https://learn.adafruit.com/pmsa003i). It was tested on Raspberry Pi 3 and a Raspberry Pi Zero W with sensor connected to I2C protocol.
 This module uses CircuitPython libraries and python-shell node.js bridge to connect to a PMSA300I sensor and read its data.
 
 ## Prerequisites
-- It uses https://github.com/adafruit/Adafruit_Blinka.
+- Adafruit Blink is required for this to run: https://github.com/adafruit/Adafruit_Blinka.
 - Instructions for Installing on a Raspberry Pi - https://learn.adafruit.com/circuitpython-on-raspberrypi-linux
 
 ## Installation
 ```
-npm --save install node-adafruit-pmsa300i
+`npm --save install node-adafruit-pmsa300i`
 ```
 
 ## Usage
 ```
-const AirQualitySensor = require('node-adafruit-pmsa300i');
+`const AirQualitySensor = require('node-adafruit-pmsa300i');
 
 AirQualitySensor.read(function(results) {
   console.log(results);
-});
+});`
+
+results is an object with the following signature:
+`{
+    pm10Standard: number,
+    pm25Standard: number,
+    pm100Standard: number,
+    pm10Env: number,
+    pm25Env: number,
+    pm100Env: number,
+    particles03um: number,
+    particles05um: number,
+    particles10um: number,
+    particles25um: number,
+    particles50um: number,
+    particles100um: number
+}`
+
 
 ```
--- index.js
+++ index.js
@@ -1,14 +1,41 @@
 var PythonShell = require('python-shell');
 
-var AirQualitySensor = function() {
+var AirQualitySensor = function () {
 
 }
 
-AirQualitySensor.prototype.read = function(cb) {
-  PythonShell.run('read.py', { scriptPath: __dirname+'/python' }, function (err, results) {
-    if (err) throw err;
-    cb.apply(this, results);
-  });
+AirQualitySensor.prototype.read = function (cb) {
+    PythonShell.run('read.py', { scriptPath: __dirname + '/python' }, function (err, results) {
+        if (err) {
+            throw err;
+        }
+
+        try {
+            if (Array.isArray(results) && results.length > 0) {
+                results = [results[0].replace(/'/g, '"')];
+                const aqVals = JSON.parse(results);
+                const airQualValsObj = {};
+
+                airQualValsObj.pm10Standard = aqVals['pm10 standard'];
+                airQualValsObj.pm25Standard = aqVals['pm25 standard'];
+                airQualValsObj.pm100Standard = aqVals['pm100 standard'];
+                airQualValsObj.pm10Env = aqVals['pm10 env'];
+                airQualValsObj.pm25Env = aqVals['pm25 env'];
+                airQualValsObj.pm100Env = aqVals['pm100 env'];
+                airQualValsObj.particles03um = aqVals['particles 03um'];
+                airQualValsObj.particles05um = aqVals['particles 05um'];
+                airQualValsObj.particles10um = aqVals['particles 10um'];
+                airQualValsObj.particles25um = aqVals['particles 25um'];
+                airQualValsObj.particles50um = aqVals['particles 50um'];
+                airQualValsObj.particles100um = aqVals['particles 100um'];
+
+                cb.call(this, airQualValsObj);
+            }
+        } catch (err) {
+            console.error(err);
+            cb.call(this, null);
+        }
+    });
 };
 
 module.exports = new AirQualitySensor();
